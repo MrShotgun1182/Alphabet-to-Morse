895ed458558e76a4cd64f5c063fb76f13415ef14
MR surface <motteza1182@gmail.com>
2025-02-23 11:45:41 +0330

    make changer in main file

--- main.py
+++ main.py
@@ -7,4 +7,21 @@ def json_reader(file: str) -> dict:
         dictionary = json.load(f)
     return dictionary
 
+def changer():
+    text = str()
+    char = str()
+    dictionary = json_reader("dictionary.json")
+    while(char != '-'):
+        system('cls')
+        print(text)
+        char = click.getchar()
+        char = char.upper()
+        text += dictionary[char] + ' '
+
+
+def main():
+    changer()
+
+if __name__ == "__main__":
+    main()
         
